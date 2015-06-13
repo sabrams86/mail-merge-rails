92498a3cb45d8cdb03795bbda983ad0c1bb52318
Steven Abrams <steven.abrams86@gmail.com>
2015-06-12 22:23:42 -0600

    when user submits form, it remains filled out

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,9 @@ class HomeController < ApplicationController
 
   def index
     @title = 'Mail Merge'
+    @recipients = params[:recipients]
+    @subject = params[:subject]
+    @body = params[:emailbody]
   end
 
 end
